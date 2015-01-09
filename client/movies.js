2303501a1d4c3a4352481758e7aff773335b9b92
Zarko Dubajic <zardubajic@gmail.com>
2015-01-10 00:29:48 +0100

    dodata mogucnost dodavanja filmova

--- client/movies.js
+++ client/movies.js
@@ -5,3 +5,19 @@ Movies = new Meteor.Collection("movies");
 Template.moviesTemplate.movies = function () {
     return Movies.find();
 };
+// Handle movieForm events
+Template.movieForm.events = {
+    'submit': function (e, tmpl) {
+        // Don't postback
+        e.preventDefault();
+ 
+        // create the new movie
+        var newMovie = {
+            title: tmpl.find("#title").value,
+            director: tmpl.find("#director").value
+        };
+ 
+        // add the movie to the db
+        Movies.insert(newMovie);
+    }
+};
